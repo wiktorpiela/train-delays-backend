bdd2cbdd1bf765bfd99cc36682a8e181c325f41a
Wiktor Piela <wpiela9@gmail.com>
2024-05-15 18:01:58 +0200

    endpoint for stations and relations

--- trains_delay_app_backend/predictor/urls.py
+++ trains_delay_app_backend/predictor/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('test/', test_view, name='test'),
+    path('stations/', StationView.as_view(), name='stations'),
+    path('relations/<int:pk>/', RelationView.as_view(), name='relations'),
 ]
