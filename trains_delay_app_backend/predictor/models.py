314d5466653a2697b12fa74a97174fbb28e57969
Wiktor Piela <wpiela9@gmail.com>
2024-05-17 08:32:16 +0200

    models in db updated

--- trains_delay_app_backend/predictor/models.py
+++ trains_delay_app_backend/predictor/models.py
@@ -1,19 +1,20 @@
 from django.contrib.gis.db import models
 
-class Relation(models.Model):
-    relation_name = models.CharField(max_length=10000)
-    relation_gps = models.LineStringField(srid=4326)
-
 class Station(models.Model):
     station_name = models.CharField(max_length=100)
     station_gps = models.PointField(srid=4326)
 
 class Route(models.Model):
-    relation = models.ForeignKey(Relation, related_name='route_relation', on_delete=models.DO_NOTHING)
-    station = models.ForeignKey(Station, related_name='route_station', on_delete=models.DO_NOTHING)
-    
-class TimeTable(models.Model):
-    relation = models.ForeignKey(Relation, related_name='timetable_relation', on_delete=models.DO_NOTHING)
-    station = models.ForeignKey(Station, related_name='timetable_station', on_delete=models.DO_NOTHING)
-    arrival_time = models.TimeField()
-    departure_time = models.TimeField()
+    route_name = models.CharField(max_length=500)
+    route_gps = models.LineStringField(srid=4326)
+    station = models.ManyToManyField(Station, related_name='route')
+
+    def fix_relation_name(self):
+        rel_name = self.relation_name.split('_')[0]
+        return rel_name
+ 
+# class TimeTable(models.Model):
+#     relation = models.ForeignKey(Relation, related_name='timetable_relation', on_delete=models.DO_NOTHING)
+#     station = models.ForeignKey(Station, related_name='timetable_station', on_delete=models.DO_NOTHING)
+#     arrival_time = models.TimeField()
+#     departure_time = models.TimeField()
