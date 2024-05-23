b4a32b3b91a86be6ff507092a9fbc5130482d35d
Wiktor Piela <wpiela9@gmail.com>
2024-05-23 16:00:46 +0200

    test view added

--- trains_delay_app_backend/predictor/views.py
+++ trains_delay_app_backend/predictor/views.py
@@ -1,11 +1,37 @@
-from rest_framework import generics
-from .models import Station, Relation
-from .api.serializers import StationSerializer, RelationSerializer
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Station, Route
+from .api.serializers import RouteSerializer
 
-class StationView(generics.ListAPIView):
-    queryset = Station.objects.all()
-    serializer_class = StationSerializer
 
-class RelationView(generics.RetrieveAPIView):
-    queryset = Relation.objects.all()
-    serializer_class = RelationSerializer
+class RouteView(APIView):
+
+    def get(self, request):
+        queryset = Route.objects.all()[0]
+        serializer = RouteSerializer(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def post(self, request, format=None):
+
+        station_name = request.data.get('station_name')
+
+        if not station_name:
+            return Response({'error': 'Station name is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            station = Station.objects.get(station_name=station_name)
+        except Station.DoesNotExist:
+            return Response({'error': 'Station not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        print(station.station_gps)
+
+        routes = Route.objects.filter(station=station)
+
+        print(routes)
+
+        serializer = RouteSerializer(routes, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
