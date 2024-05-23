41fa55a9ef3d8db4018dc3cfc41774134af6c0dc
Wiktor Piela <wpiela9@gmail.com>
2024-05-23 16:00:13 +0200

    test endpoint added

--- trains_delay_app_backend/predictor/urls.py
+++ trains_delay_app_backend/predictor/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('stations/', StationView.as_view(), name='stations'),
-    path('relations/<int:pk>/', RelationView.as_view(), name='relations'),
+    path('test/', RouteView.as_view(), name='routes'),
 ]
