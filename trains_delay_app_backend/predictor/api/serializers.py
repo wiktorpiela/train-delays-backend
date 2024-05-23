8027a2c1f7077b13edae7ae34588eb89c8fb5a63
Wiktor Piela <wpiela9@gmail.com>
2024-05-23 15:58:43 +0200

    timetable serializer added and serializers relation redisigned

--- trains_delay_app_backend/predictor/api/serializers.py
+++ trains_delay_app_backend/predictor/api/serializers.py
@@ -1,17 +1,35 @@
 from rest_framework import serializers
-from ..models import Station, Relation
+from ..models import Station, Route, Timetable
+from django.contrib.gis.geos import Point
+from .fields import UniqueListField
 
 class StationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Station
-        fields = '__all__'
+        fields = ('station_name',)
 
-class RelationSerializer(serializers.ModelSerializer):
+class TimetableSerializer(serializers.ModelSerializer):
+    station = serializers.StringRelatedField()
+    station_gps = serializers.SerializerMethodField()
+
     class Meta:
-        model = Relation
-        fields = '__all__'
+        model = Timetable
+        fields = ('id', 'station', 'arrival_time', 'departure_time', 'station_gps')
+
+    def get_station_gps(self, obj):
+        return {
+            'lat': obj.station.station_gps.y,
+            'lon': obj.station.station_gps.x,
+            }
+
+class RouteSerializer(serializers.ModelSerializer):
+    timetable_relation = TimetableSerializer(many=True)
+
+    class Meta:
+        model = Route
+        fields = ('id', 'route_name', 'timetable_relation', 'route_gps')
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        representation['relation_name'] = instance.relation_name.split('_')[0]
+        representation['route_name'] = instance.route_name.split('_')[0]
         return representation
