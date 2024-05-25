c2f92bb6b07358804a35710094bded20b1624127
Wiktor Piela <wpiela9@gmail.com>
2024-05-25 19:37:40 +0200

    routers added

--- trains_delay_app_backend/predictor/urls.py
+++ trains_delay_app_backend/predictor/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from .views import *
 
+router = routers.DefaultRouter()
+router.register('stations', StationView, basename='all-stations')
+
 urlpatterns = [
-    path('test/', RouteView.as_view(), name='routes'),
+    #path('test/', RouteView.as_view(), name='routes'),
+    path('stations/<int:pk>/', StationByRouteView.as_view(), name='routes'),
+    path('', include(router.urls)),
 ]
