5f71d606ed9a1d3ae49201435fa2dd7fa582cb31
Wiktor Piela <wpiela9@gmail.com>
2024-05-29 21:32:58 +0200

    import data to db from files scripts updated

--- trains_delay_app_backend/predictor/management/commands/import_timetables_schedules.py
+++ trains_delay_app_backend/predictor/management/commands/import_timetables_schedules.py
@@ -1,55 +1,47 @@
 from django.core.management.base import BaseCommand, CommandError
 from predictor.models import Route, Timetable, Station, Schedule
+from predictor.utils.import_data_utils import filter_csv_dict
 import csv, os
 
 class Command(BaseCommand):
     help = 'Imports data from a CSV file into the MyModel model.'
 
     def add_arguments(self, parser):
-        parser.add_argument('timetable_file_path', type=str)
         parser.add_argument('schedule_file_path', type=str)
 
     def handle(self, *args, **kwargs):
-        timetable_path = kwargs['timetable_file_path']
         schedule_path = kwargs['schedule_file_path']
-
-        if not os.path.exists(timetable_path):
-            raise CommandError(f'CSV file "{timetable_path}" does not exist')
         
-        elif not os.path.exists(schedule_path):
+        if not os.path.exists(schedule_path):
             raise CommandError(f'CSV file "{schedule_path}" does not exist')
         
-        self.import_data_from_csv(timetable_path, schedule_path)
+        self.import_data_from_csv(schedule_path)
 
-    def import_data_from_csv(self, timetable_path, schedule_path):
-        with open(timetable_path, 'r') as timetablefile:
-            reader = csv.DictReader(timetablefile)
+    def import_data_from_csv(self, schedule_path):
+        routes = Route.objects.all()
 
-            for row in reader:
-                route_id = row['route'] 
-                route_obj = Route.objects.get(id=route_id)
-                timetable_instance = Timetable.objects.create(route = route_obj)
+        for route in routes:
+            timetable_instance = Timetable.objects.create(route=route)
 
-                with open(schedule_path, 'r') as schedulefile:
-                    subreader = csv.DictReader(schedulefile)
+            filter_criteria = {'route_id': str(route.id)}
+            with open(schedule_path, 'r') as schedulefile:
+                reader = csv.DictReader(schedulefile)
+            
+                rows = filter_csv_dict(reader, filter_criteria)
+                for row in rows:
+                    sub_station_id = row['station_id']
+                    arrival_time = row['arrival_time']
+                    departure_time = row['departure_time']
 
-                    for subrow in subreader:
-                        sub_route_id = subrow['route_id']
+                    station_instance = Station.objects.get(id=sub_station_id)
 
-                        if sub_route_id == route_id:
-                            sub_station_id = subrow['station_id']
-                            arrival_time = subrow['arrival_time']
-                            departure_time = subrow['departure_time']
+                    new_schedule_instance = Schedule.objects.create(
+                        timetable = timetable_instance,
+                        station = station_instance,
+                        arrival_time = arrival_time,
+                        departure_time = departure_time
+                    )
 
-                            station_instance = Station.objects.get(id=sub_station_id)
+                    new_schedule_instance.save()
 
-                            new_schedule_instance = Schedule.objects.create(
-                                timetable = timetable_instance,
-                                station = station_instance,
-                                arrival_time = arrival_time,
-                                departure_time = departure_time
-                            )
-
-                            new_schedule_instance.save()
-
-                timetable_instance.save()
+            timetable_instance.save()
