cc5b8ccb0e2194f80f4d2653949d56a1582bf66a
Wiktor Piela <wpiela9@gmail.com>
2024-05-29 21:34:35 +0200

    new urls and views

--- trains_delay_app_backend/predictor/urls.py
+++ trains_delay_app_backend/predictor/urls.py
@@ -2,11 +2,10 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import *
 
-# router = routers.DefaultRouter()
-# router.register('stations', StationView, basename='all-stations')
+router = routers.DefaultRouter()
+router.register('stations', StationView, basename='all-stations')
 
 urlpatterns = [
-    #path('test/', RouteView.as_view(), name='routes'),
-    # path('stations/<int:pk>/', StationByRouteView.as_view(), name='routes'),
+    path('routes/<int:pk>/', StationByRouteView.as_view(), name='routes'),
     # path('', include(router.urls)),
 ]
--- trains_delay_app_backend/predictor/views.py
+++ trains_delay_app_backend/predictor/views.py
@@ -2,58 +2,62 @@ from rest_framework import generics, status, viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-# from .models import Station, Route, Timetable
-# from .api.serializers import RouteSerializer, StationSerializer, TimetableSerializer
+from .models import Station, Route
+from .api.serializers import StationSerializer, RouteSerializer
 
-# from .utils.locked_stations import LOCKED_STATIONS
+from .utils.locked_stations import LOCKED_STATIONS
 
-# class StationView(viewsets.ReadOnlyModelViewSet):
-#     serializer_class = StationSerializer
+class StationView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = StationSerializer
 
-#     def get_queryset(self):
-#         ids_to_exclude = [item[0] for item in LOCKED_STATIONS]
-#         queryset = Station.objects.exclude(id__in=ids_to_exclude)
-#         return queryset
+    def get_queryset(self):
+        ids_to_exclude = [item[0] for item in LOCKED_STATIONS]
+        queryset = Station.objects.exclude(id__in=ids_to_exclude)
+        return queryset
+       
+class StationByRouteView(APIView):
 
-# class StationByRouteView(APIView):
+    def get(self, request, pk):
+        start_station = Station.objects.get(id=pk)
+        routes = Route.objects.filter(stations=start_station)
 
-#     def get(self, request, pk):
-#         start_station = Station.objects.get(id=pk)
-#         routes = Route.objects.filter(station=start_station)
+        # wektoryzacja na np array ------
+        stations_b = []
+        for route in routes:
+            stations = route.stations.all()
+            for station in stations:
+                current_name = station.station_name
+                if current_name not in stations_b:
+                    stations_b.append(current_name)
 
-#         # wektoryzacja na np array ------
-#         stations_b = []
-#         for route in routes:
-#             stations = route.station.all()
-#             for station in stations:
-#                 current_name = station.station_name
-#                 if current_name not in stations_b:
-#                     stations_b.append(current_name)
-
-#         route_names_list = []
-#         stations = []
+        route_names_list = []
+        stations = []
         
-#         # ['Węgliniec - Wrocław Główny', 'sadasd', 'asdasd']
-#         # [['a', 'b', 'c', 'd'], ['x','y','z'], [],]
-#         for route in routes:
-#             print(f"{route.route_name.split('_')[0]} ------------- ")
-#             all_stations = route.station.all()
-#             for station in all_stations:
-#                 print(station.station_name)
+        # ['Węgliniec - Wrocław Główny', 'sadasd', 'asdasd']
+        # [['a', 'b', 'c', 'd'], ['x','y','z'], [],]
+        for route in routes:
+            print(f"{route.route_name.split('_')[0]} ------------- ")
+            all_stations = route.stations.all()
+            for station in all_stations:
+                print(station.station_name)
 
-#         # serializer = RouteSerializer(routes, many=True)
-#         return Response({'stations': sorted(stations_b)}, 
-#                         #  'routes': [
-#                         #      {
-#                         #          'route_name': 'Węgliniec - Wrocław Główny',
-#                         #          'route_stations': ['Wrocław Główny', 'Malczyce', ....]
-#                         #      },
-#                         #      {
-#                         #         'route_name': 'Węgliniec - Wrocław Główny',
-#                         #         'route_stations': ['Wrocław Główny', 'Malczyce', ....]
-#                         #      }
-#                         #  ]
-#                         #  }
-#                          )
+        serializer = RouteSerializer(routes, many=True, fields=('route_name','stations',))
+        return Response(serializer.data)
+        # return Response({'stations': sorted(stations_b)
+                         
+                         
+        #                  }, 
+                        #  'routes': [
+                        #      {
+                        #          'route_name': 'Węgliniec - Wrocław Główny',
+                        #          'route_stations': ['Wrocław Główny', 'Malczyce', ....]
+                        #      },
+                        #      {
+                        #         'route_name': 'Węgliniec - Wrocław Główny',
+                        #         'route_stations': ['Wrocław Główny', 'Malczyce', ....]
+                        #      }
+                        #  ]
+                        #  }
+
     
 
