96edb8476645146c52ab3bf51637462b007581ca
Wiktor Piela <wpiela9@gmail.com>
2024-05-29 21:33:46 +0200

    serializers for new db desing

--- trains_delay_app_backend/predictor/api/serializers.py
+++ trains_delay_app_backend/predictor/api/serializers.py
@@ -1,44 +1,36 @@
 from rest_framework import serializers
-# from ..models import Station, Route, Timetable
-# from django.contrib.gis.geos import Point
+from ..models import Station, Route
+from django.contrib.gis.geos import Point
 
-# class StationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Station
-#         fields = ('id', 'station_name', 'station_gps',)
+class StationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Station
+        fields = ('id', 'station_name', 'station_gps',)
 
-# class TimetableSerializer(serializers.ModelSerializer):
-#     station = serializers.StringRelatedField()
-#     station_gps = serializers.SerializerMethodField()
+class RouteSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        fields = kwargs.pop('fields', None)
+        super().__init__(*args, **kwargs)
 
-#     class Meta:
-#         model = Timetable
-#         fields = ('id', 'station', 'arrival_time', 'departure_time', 'station_gps')
+        if fields is not None:
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
 
-#     def get_station_gps(self, obj):
-#         return {
-#             'lat': obj.station.station_gps.y,
-#             'lon': obj.station.station_gps.x,
-#             }
+    stations = serializers.SerializerMethodField()
 
-# class RouteSerializer(serializers.ModelSerializer):
-#     def __init__(self, *args, **kwargs):
-#         fields = kwargs.pop('fields', None)
-#         super().__init__(*args, **kwargs)
+    class Meta:
+        model = Route
+        fields = '__all__'
 
-#         if fields is not None:
-#             allowed = set(fields)
-#             existing = set(self.fields)
-#             for field_name in existing - allowed:
-#                 self.fields.pop(field_name)
+    def get_stations(self, obj):
+        unique_stations = []
+        for station in obj.stations.all():
+            current_name = station.station_name
+            if current_name not in unique_stations:
+                unique_stations.append(current_name)
+        return unique_stations
 
-#     timetable_relation = TimetableSerializer(many=True)
 
-#     class Meta:
-#         model = Route
-#         fields = ('id', 'route_name', 'timetable_relation', 'route_gps')
-
-#     def to_representation(self, instance):
-#         representation = super().to_representation(instance)
-#         representation['route_name'] = instance.route_name.split('_')[0]
-#         return representation
+    
