8b8cd27cd43d064ec3ccd71f44e6f66a30c20a17
Wiktor Piela <wpiela9@gmail.com>
2024-05-29 21:34:12 +0200

    new db design

--- trains_delay_app_backend/predictor/models.py
+++ trains_delay_app_backend/predictor/models.py
@@ -10,6 +10,7 @@ class Station(models.Model):
 class Route(models.Model):
     route_name = models.CharField(max_length=500)
     route_gps = models.LineStringField(srid=4326)
+    stations = models.ManyToManyField(Station, through='Schedule', related_name='routes')
 
     def fix_route_name(self):
         route_name = self.route_name.split('_')[0]
@@ -18,15 +19,13 @@ class Route(models.Model):
     def __str__(self) -> str:
         return self.route_name
     
-class Timetable(models.Model):
-    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='timetables')
-    stations = models.ManyToManyField(Station, through='Schedule', related_name='timetables')
+class Schedule(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='schedule_route')
+    station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='schedule_station')
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ('order',)
 
     def __str__(self) -> str:
-        return f'{self.route.route_name} - {self.stations.station_name}'
-
-class Schedule(models.Model):
-    timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE, related_name='schedules')
-    station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='schedules')
-    arrival_time = models.TimeField()
-    departure_time = models.TimeField()
+        return f'{self.route.route_name} - {self.station.station_name}'
